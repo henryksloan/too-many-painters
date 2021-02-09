c335074cf3ac398b2d0e1c29c8af573ac87c3fde
Henry Sloan <henryksloan@gmail.com>
2021-02-09 13:01:19 -0500

    Fix naming convention only in backend

--- index.js
+++ index.js
@@ -46,58 +46,58 @@ function shuffle(array) {
 };
 
 let rooms = {};
-let user_rooms = {};
-const ink_max = 100, ink_min = 35;
+let userRooms = {};
+const inkMax = 100, inkMin = 35;
 // TODO: Should white be here as a funny eraser? Maybe make it rare and special on the frontend (like an eraser)?
 const colors = ['red', 'blue', 'green', 'black', 'cyan', 'darkred', 'darkgreen'];
 
-function round_start(room) {
+function roundStart(room) {
   if (!rooms[room]) return;
 
   io.to(room).emit('round_started', {
     guesser: rooms[room].guesser,
-    paint_order: rooms[room].paint_order
+    paintOrder: rooms[room].paintOrder
   });
   // TODO: Round counter, limit, round times should be in the room object
   // TODO: Maybe account for latency by adding some time
-  setTimeout(() => { start_draw(room) }, 3000);
+  setTimeout(() => { startDraw(room) }, 3000);
 }
 
-function start_draw(room) {
+function startDraw(room) {
   if (!rooms[room]) return;
 
   console.log(rooms[room]);
-  const ink_amount = Math.floor(Math.random() * ((ink_max + 1) - ink_min) + ink_min);
+  const inkAmount = Math.floor(Math.random() * ((inkMax + 1) - inkMin) + inkMin);
   const color = colors[Math.floor(Math.random() * colors.length)];
-  rooms[room].ink_amount = ink_amount;
+  rooms[room].inkAmount = inkAmount;
   rooms[room].color = color;
-  io.to(room).emit('start_draw', { painter: rooms[room].painter, inkAmount: ink_amount, color });
+  io.to(room).emit('start_draw', { painter: rooms[room].painter, inkAmount: inkAmount, color });
   io.to(rooms[room].painter).emit('your_turn');
 
-  setTimeout(() => { times_up(room) }, 5000);
+  setTimeout(() => { timesUp(room) }, 5000);
 }
 
-function times_up(room) {
+function timesUp(room) {
   if (!rooms[room]) return;
 
-  painter_index = rooms[room].paint_order.indexOf(rooms[room].painter);
+  let painterIndex = rooms[room].paintOrder.indexOf(rooms[room].painter);
   rooms[room].painter = null;
-  if (painter_index < rooms[room].paint_order.length - 1) {
-    rooms[room].painter = rooms[room].paint_order[painter_index + 1];
-    start_draw(room);
+  if (painterIndex < rooms[room].paintOrder.length - 1) {
+    rooms[room].painter = rooms[room].paintOrder[painterIndex + 1];
+    startDraw(room);
   } else {
     console.log("Round end");
     // TODO: Maybe extra guess time?
     rooms[room].lines = [];
 
-    guesser_index = rooms[room].guess_order.indexOf(rooms[room].guesser);
-    if (guesser_index < rooms[room].guess_order.length - 1) {
-      rooms[room].guesser = rooms[room].guess_order[guesser_index + 1];
-      rooms[room].paint_order = shuffle(rooms[room].users.slice());
-      const index = rooms[room].paint_order.indexOf(rooms[room].guesser);
-      rooms[room].paint_order.splice(index, 1);
-      rooms[room].painter = rooms[room].paint_order[0];
-      round_start(room);
+    let guesserIndex = rooms[room].guessOrder.indexOf(rooms[room].guesser);
+    if (guesserIndex < rooms[room].guessOrder.length - 1) {
+      rooms[room].guesser = rooms[room].guessOrder[guesserIndex + 1];
+      rooms[room].paintOrder = shuffle(rooms[room].users.slice());
+      const index = rooms[room].paintOrder.indexOf(rooms[room].guesser);
+      rooms[room].paintOrder.splice(index, 1);
+      rooms[room].painter = rooms[room].paintOrder[0];
+      roundStart(room);
     } else {
       // TODO: What to do when number of rounds exceeds number of players to guess? Probably just loop through.
       console.log("Game end?")
@@ -107,31 +107,31 @@ function times_up(room) {
 
 io.on('connection', (socket) => {
   console.log(`${socket.id} connected`);
-  user_rooms[socket.id] = null;
+  userRooms[socket.id] = null;
 
   socket.on('disconnect', () => {
     console.log(`${socket.id} disconnected`);
-    if (user_rooms[socket.id]) {
-      const index = rooms[user_rooms[socket.id]].users.indexOf(socket.id);
-      rooms[user_rooms[socket.id]].users.splice(index, 1);
+    if (userRooms[socket.id]) {
+      const index = rooms[userRooms[socket.id]].users.indexOf(socket.id);
+      rooms[userRooms[socket.id]].users.splice(index, 1);
       // TODO: players_changed should send more limited info, including new game state (especially drawers, etc). Also, what if it interrupts a round?
-      socket.to(user_rooms[socket.id]).emit('players_changed', rooms[user_rooms[socket.id]]);
-      if (rooms[user_rooms[socket.id]].users.length === 0) {
-        console.log(`deleting room ${user_rooms[socket.id]}`)
-        delete rooms[user_rooms[socket.id]];
+      socket.to(userRooms[socket.id]).emit('players_changed', rooms[userRooms[socket.id]]);
+      if (rooms[userRooms[socket.id]].users.length === 0) {
+        console.log(`deleting room ${userRooms[socket.id]}`)
+        delete rooms[userRooms[socket.id]];
       }
-      delete user_rooms[socket.id];
+      delete userRooms[socket.id];
     }
   });
 
   socket.on('create_room', () => {
     let id = makeID(idLength);
     rooms[id] = { users: [], lines: [],
-      started: false, play_started: false,
-      guess_order: null, guesser: null,
-      paint_order: null, painter: null,
-      ink_amount: 0, color: 'red',
-      users_loading: [] };
+      started: false, playStarted: false,
+      guessOrder: null, guesser: null,
+      paintOrder: null, painter: null,
+      inkAmount: 0, color: 'red',
+      usersLoading: [] };
     socket.join(id);
     socket.emit('room_created', id);
   });
@@ -141,23 +141,23 @@ io.on('connection', (socket) => {
       socket.emit('exception', {errorMessage: 'No such room exists'});
       return;
     }
-    if (user_rooms[socket.id]) {
-      rooms[user_rooms[socket.id]].users_loading = rooms[user_rooms[socket.id]].users_loading.filter(x => x != socket.id);
-      console.log(`${socket.id} finished loading into room ${user_rooms[socket.id]}`);
-      if (rooms[user_rooms[socket.id]].users_loading.length == 0 && !rooms[user_rooms[socket.id]].play_started) {
+    if (userRooms[socket.id]) {
+      rooms[userRooms[socket.id]].usersLoading = rooms[userRooms[socket.id]].usersLoading.filter(x => x != socket.id);
+      console.log(`${socket.id} finished loading into room ${userRooms[socket.id]}`);
+      if (rooms[userRooms[socket.id]].usersLoading.length == 0 && !rooms[userRooms[socket.id]].playStarted) {
         // TODO: Is this a race condition that won't run if two people finish loading at the same time?
         // TODO: Make some interface for this loading on the frontend
-        rooms[user_rooms[socket.id]].play_started = true;
-        round_start(user_rooms[socket.id]);
+        rooms[userRooms[socket.id]].playStarted = true;
+        roundStart(userRooms[socket.id]);
       }
-      // socket.emit('rooms[user_rooms[socket.id]].users_loadingexception', {errorMessage: 'User already in a room'});
+      // socket.emit('rooms[userRooms[socket.id]].usersLoadingexception', {errorMessage: 'User already in a room'});
       return;
     }
     console.log(`${socket.id} joining room ${room}`);
     socket.join(room);
     rooms[room].users.push(socket.id);
-    // TODO: Probably inject them into guess_order if game started. Probably at end?.
-    user_rooms[socket.id] = room;
+    // TODO: Probably inject them into guessOrder if game started. Probably at end?.
+    userRooms[socket.id] = room;
     socket.emit('initialize', { users: rooms[room].users,
       lines: rooms[room].lines });
     socket.to(room).emit('players_changed', rooms[room]);
@@ -165,61 +165,61 @@ io.on('connection', (socket) => {
   });
 
   socket.on('initialize', () => {
-    const room_id = user_rooms[socket.id];
-    if (room_id && rooms[room_id]) {
+    const roomId = userRooms[socket.id];
+    if (roomId && rooms[roomId]) {
       socket.emit('initialize', {
-        users: rooms[room_id].users,
-        lines: rooms[room_id].lines,
-        guesser: rooms[room_id].guesser,
-        paint_order: rooms[room_id].paint_order,
-        painter: rooms[room_id].painter,
-        color: rooms[room_id].color,
-        inkAmount: rooms[room_id].ink_amount,
+        users: rooms[roomId].users,
+        lines: rooms[roomId].lines,
+        guesser: rooms[roomId].guesser,
+        paintOrder: rooms[roomId].paintOrder,
+        painter: rooms[roomId].painter,
+        color: rooms[roomId].color,
+        inkAmount: rooms[roomId].inkAmount,
       });
     }
   })
 
   socket.on('leave_room', () => {
-    if (user_rooms[socket.id]) {
-      socket.leave(user_rooms[socket.id]);
-      console.log(`${socket.id} leaving room ${user_rooms[socket.id]}`);
-      const index = rooms[user_rooms[socket.id]].users.indexOf(socket.id);
-      rooms[user_rooms[socket.id]].users.splice(index, 1);
-      socket.to(user_rooms[socket.id]).emit('players_changed', rooms[user_rooms[socket.id]]);
-      if (rooms[user_rooms[socket.id]].users.length === 0) {
-        console.log(`deleting room ${user_rooms[socket.id]}`)
-        delete rooms[user_rooms[socket.id]];
+    if (userRooms[socket.id]) {
+      socket.leave(userRooms[socket.id]);
+      console.log(`${socket.id} leaving room ${userRooms[socket.id]}`);
+      const index = rooms[userRooms[socket.id]].users.indexOf(socket.id);
+      rooms[userRooms[socket.id]].users.splice(index, 1);
+      socket.to(userRooms[socket.id]).emit('players_changed', rooms[userRooms[socket.id]]);
+      if (rooms[userRooms[socket.id]].users.length === 0) {
+        console.log(`deleting room ${userRooms[socket.id]}`)
+        delete rooms[userRooms[socket.id]];
       }
-      delete user_rooms[socket.id];
+      delete userRooms[socket.id];
     }
   });
 
   socket.on('start_room', () => {
-    const room_id = user_rooms[socket.id];
-    if (room_id
-      && rooms[room_id]
-      && rooms[room_id].users[0] === socket.id) {
-      rooms[user_rooms[socket.id]].lines = [];
-      rooms[user_rooms[socket.id]].guess_order =
-        shuffle(rooms[user_rooms[socket.id]].users.slice());
-      rooms[user_rooms[socket.id]].guesser =
-        rooms[user_rooms[socket.id]].guess_order[0];
-      rooms[user_rooms[socket.id]].paint_order =
-        shuffle(rooms[user_rooms[socket.id]].users.slice());
-      const index = rooms[user_rooms[socket.id]].paint_order
-        .indexOf(rooms[user_rooms[socket.id]].guesser);
-      rooms[user_rooms[socket.id]].paint_order.splice(index, 1);
-      rooms[user_rooms[socket.id]].painter =
-        rooms[user_rooms[socket.id]].paint_order[0];
-      rooms[user_rooms[socket.id]].users_loading = rooms[user_rooms[socket.id]].users.slice();
-      rooms[user_rooms[socket.id]].started = true;
-      console.log("Room started", rooms[user_rooms[socket.id]]);
-      io.to(room_id).emit('room_started');
+    const roomId = userRooms[socket.id];
+    if (roomId
+      && rooms[roomId]
+      && rooms[roomId].users[0] === socket.id) {
+      rooms[userRooms[socket.id]].lines = [];
+      rooms[userRooms[socket.id]].guessOrder =
+        shuffle(rooms[userRooms[socket.id]].users.slice());
+      rooms[userRooms[socket.id]].guesser =
+        rooms[userRooms[socket.id]].guessOrder[0];
+      rooms[userRooms[socket.id]].paintOrder =
+        shuffle(rooms[userRooms[socket.id]].users.slice());
+      const index = rooms[userRooms[socket.id]].paintOrder
+        .indexOf(rooms[userRooms[socket.id]].guesser);
+      rooms[userRooms[socket.id]].paintOrder.splice(index, 1);
+      rooms[userRooms[socket.id]].painter =
+        rooms[userRooms[socket.id]].paintOrder[0];
+      rooms[userRooms[socket.id]].usersLoading = rooms[userRooms[socket.id]].users.slice();
+      rooms[userRooms[socket.id]].started = true;
+      console.log("Room started", rooms[userRooms[socket.id]]);
+      io.to(roomId).emit('room_started');
     }
   });
 
   socket.on('draw', (coords) => {
-    const room = user_rooms[socket.id];
+    const room = userRooms[socket.id];
     if (room && rooms[room].started && rooms[room].painter === socket.id) {
       socket.to(room).emit('draw', { coords: coords, color: rooms[room].color });
       rooms[room].lines.push(coords); // TODO: Lines should include color
